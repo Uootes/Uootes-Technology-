2c8bc74888ba3da6e3c3a3a474dd0c6319fc9040
Frank emesinwa <frankemesinwa@gmail.com>
2024-09-16 14:57:29 +0100

    added the entire folder

---
+++ Uootes Technologies/script.js
@@ -0,0 +1,134 @@
+document.getElementById('dropbtn1').addEventListener('click', function(){
+  
+document.getElementById('faqitem1').style.height = '180px';
+document.getElementById('closedropbtn1').style.display = 'block';
+document.getElementById('dropbtn1').style.display = 'none';
+document.getElementById('droptext1').style.display = 'block';
+
+/*document.querySelectorAll('.faq-item').style.display = 'block';
+document.getElementById('droptext').style.animation = 'fadein';*/
+});
+
+
+document.getElementById('dropbtn2').addEventListener('click', function(){
+document.getElementById('droptext2').style.display = 'block'; 
+document.getElementById('faqitem2').style.height = '180px';
+document.getElementById('closedropbtn2').style.display = 'block';
+document.getElementById('dropbtn2').style.display = 'none';
+
+    
+    /*document.querySelectorAll('.faq-item').style.display = 'block';
+    document.getElementById('droptext').style.animation = 'fadein';*/
+});
+    
+    
+
+document.getElementById('dropbtn3').addEventListener('click', function(){
+document.getElementById('droptext3').style.display = 'block';
+ document.getElementById('faqitem3').style.height = '180px';
+ document.getElementById('closedropbtn3').style.display = 'block';
+ document.getElementById('dropbtn3').style.display = 'none';
+
+        /*document.querySelectorAll('.faq-item').style.display = 'block';
+        document.getElementById('droptext').style.animation = 'fadein';*/
+});
+       
+        document.getElementById('dropbtn4').addEventListener('click', function(){
+        document.getElementById('droptext4').style.display = 'block';
+        document.getElementById('faqitem4').style.height = '180px';
+        document.getElementById('closedropbtn4').style.display = 'block';
+        document.getElementById('dropbtn4').style.display = 'none';
+
+            /*document.querySelectorAll('.faq-item').style.display = 'block';
+            document.getElementById('droptext').style.animation = 'fadein';*/
+            });
+
+
+                document.getElementById('dropbtn5').addEventListener('click', function(){
+                document.getElementById('droptext5').style.display = 'block';
+                document.getElementById('faqitem5').style.height = '180px';
+                document.getElementById('closedropbtn5').style.display = 'block';
+                document.getElementById('dropbtn5').style.display = 'none';
+
+                /*document.querySelectorAll('.faq-item').style.display = 'block';
+                document.getElementById('droptext').style.animation = 'fadein';*/
+                });
+                
+                
+                 document.getElementById('dropbtn6').addEventListener('click', function(){
+                 document.getElementById('droptext6').style.display = 'block';
+                 document.getElementById('faqitem6').style.height = '180px';
+                 document.getElementById('closedropbtn6').style.display = 'block';
+                 document.getElementById('dropbtn6').style.display = 'none';
+
+                /*document.querySelectorAll('.faq-item').style.display = 'block';
+                document.getElementById('droptext').style.animation = 'fadein';*/
+                });
+
+                    document.getElementById('dropbtn7').addEventListener('click', function(){
+                    document.getElementById('droptext7').style.display = 'block';
+                    document.getElementById('faqitem7').style.height = '180px';
+                    document.getElementById('closedropbtn7').style.display = 'block';
+                    document.getElementById('dropbtn7').style.display = 'none';
+
+                    /*document.querySelectorAll('.faq-item').style.display = 'block';
+                    document.getElementById('droptext').style.animation = 'fadein';*/
+});
+
+
+
+/*close section for drop button */
+
+document.getElementById('closedropbtn1').addEventListener('click', function(){
+ document.getElementById('closedropbtn1').style.display = 'none';
+ document.getElementById('dropbtn1').style.display = 'block';
+ document.getElementById('droptext1').style.display = 'none';
+ document.getElementById('faqitem1').style.height = '40px';
+});
+
+
+document.getElementById('closedropbtn2').addEventListener('click', function(){
+    document.getElementById('closedropbtn2').style.display = 'none';
+    document.getElementById('dropbtn2').style.display = 'block';
+    document.getElementById('droptext2').style.display = 'none';
+    document.getElementById('faqitem2').style.height = '40px';
+});
+            
+document.getElementById('closedropbtn3').addEventListener('click', function(){
+    document.getElementById('closedropbtn3').style.display = 'none';
+    document.getElementById('dropbtn3').style.display = 'block';
+    document.getElementById('droptext3').style.display = 'none';
+    document.getElementById('faqitem3').style.height = '40px';
+   });
+
+
+   document.getElementById('closedropbtn4').addEventListener('click', function(){
+    document.getElementById('closedropbtn4').style.display = 'none';
+    document.getElementById('dropbtn4').style.display = 'block';
+    document.getElementById('droptext4').style.display = 'none';
+    document.getElementById('faqitem4').style.height = '40px';
+   });
+                    
+
+   document.getElementById('closedropbtn5').addEventListener('click', function(){
+    document.getElementById('closedropbtn5').style.display = 'none';
+    document.getElementById('dropbtn5').style.display = 'block';
+    document.getElementById('droptext5').style.display = 'none';
+    document.getElementById('faqitem5').style.height = '40px';
+   });
+
+
+   document.getElementById('closedropbtn6').addEventListener('click', function(){
+    document.getElementById('closedropbtn6').style.display = 'none';
+    document.getElementById('dropbtn6').style.display = 'block';
+    document.getElementById('droptext6').style.display = 'none';
+    document.getElementById('faqitem6').style.height = '40px';
+   });
+
+
+   document.getElementById('closedropbtn7').addEventListener('click', function(){
+    document.getElementById('closedropbtn7').style.display = 'none';
+    document.getElementById('dropbtn7').style.display = 'block';
+    document.getElementById('droptext7').style.display = 'none';
+    document.getElementById('faqitem7').style.height = '40px';
+   });
