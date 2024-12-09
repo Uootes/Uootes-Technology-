79989d06636b98c9956c4edbd6bc2052a6367731
Daramola Kehinde <daramolakenny18@gmail.com>
2024-12-09 15:27:30 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import Navbar from './component/Navbar';
 import Login from './component/Login';
 import Signup from './component/Signup';
 import Home from './pages/Home';
+import About from './pages/About';
+import Roadmap from './pages/Roadmap';
+import Howitworks from './pages/Howitworks';
+import Faq from './pages/Faq';
+import Contact from './pages/Contact';
+import Hero from './pages/Hero';
+import Services from './pages/Services';
+import Digital from './pages/Digital';
 import Footer from './component/Footer';
 
 const App = () => {
@@ -15,6 +23,14 @@ const App = () => {
         <Route path="/Login" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
       </Routes>
+      <Hero />
+      <About />
+      <Services />
+      <Roadmap />
+      <Digital />
+      <Howitworks />
+      <Faq />
+      <Contact />
       <Footer />    
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDom from 'react-dom/client';
+import  ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom'
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-ReactDom.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <App />
